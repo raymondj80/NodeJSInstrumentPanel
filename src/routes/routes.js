26c7e5f753ecb8ebd44d3cd3df187af91ca60f85
Raymond Jow <55035695+raymondj80@users.noreply.github.com>
2021-09-22 15:04:16 -0400

    added ejs engine with routes and authentication

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,50 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 
 datapacket = {};
 
-router.get('/', (req, res) => res.send('Welcome'));
+router.get("/", (req, res) => res.render("welcome"));
 
-router.get("/home", function(req, res) {
-    res.sendFile( path.join(__dirname, "../views/home.html"));
+router.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/home.html"));
 });
 
-router.get("/graphs", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/graphs.html"));
+router.get("/graphs", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/graphs.html"));
 });
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-
 router.get(
-    "/gd",
-    async function(req, res, next) {
-            res.sendFile(path.join(__dirname, "../views/gd.html"));
-            await sleep(500);
-            next();
-        },
-        function(req, res, next) {
-            if (datapacket["id"] != null) {
-                StreamData.findById(datapacket["id"]).then((res) => {
-                    mongoData = res.toJSON().record_data;
-                    console.log(mongoData);
-                    io.emit("mongoData", mongoData);
-                });
-            }
-        }
+  "/gd",
+  async function (req, res, next) {
+    res.sendFile(path.join(__dirname, "../views/gd.html"));
+    await sleep(500);
+    next();
+  },
+  function (req, res, next) {
+    if (datapacket["id"] != null) {
+      StreamData.findById(datapacket["id"]).then((res) => {
+        mongoData = res.toJSON().record_data;
+        console.log(mongoData);
+        io.emit("mongoData", mongoData);
+      });
+    }
+  }
 );
 
-router.get("/chartjs-plugin.js", function(req, res) {
-    res.sendFile(path.join(__dirname, "../plugins/chartjs-plugin-zoom.min.js"));
+router.get("/chartjs-plugin.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "../plugins/chartjs-plugin-zoom.min.js"));
 });
 
-router.get("/vue-color.min.js", function(req, res) {
-    res.sendFile(path.join(__dirname, "../plugins/vue-color.min.js"));
+router.get("/vue-color.min.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "../plugins/vue-color.min.js"));
 });
 
 // module.exports = function(req, res, next) {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,45 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const path = require('path');
+const passport = require("passport");
+const path = require("path");
 
 // User model
-const User = require('../models/user')
+const User = require("../models/user");
+const { forwardAuthenticated } = require("../config/auth");
 
 // Login Page
-router.get('/login', (req, res) => res.sendFile(path.join(__dirname,"../views/login.html")));
+router.get("/login", forwardAuthenticated, (req, res) => res.render("login"));
 
 // Register Page
-router.get('/register', (req, res) => res.sendFile(path.join(__dirname,"../views/register.html")));
+router.get("/register", forwardAuthenticated, (req, res) =>
+  res.render("register")
+);
 
 // Register Handle
-router.post('/register', async (req, res) => {
-	var newUser = new User({
-        email: req.body.email,
-        name: req.body.name,
-        groupkey: req.body.groupkey
-    });
-    newUser.setPassword(req.body.password);
-    await newUser
-        .save()
-        .then(() => {
-            var redir = { redirect: '/login' };
-            return res.json(redir);
-        })
-        .catch(err => {
-            console.log("error is ", err.message);
-            var redir = { redirect: '/error' };
-            return res.json(redir);
-        })
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
 
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please enter all fields" });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: "Password must be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        errors.push({ msg: "Email already exists" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        newUser.setPassword(req.body.password);
+        newUser.save().catch((err) => {
+          req.flash("error_msg", err);
+        });
+        req.flash("success_msg", "You are now registered and can log in");
+        res.redirect("/users/login");
+      }
+    });
+  }
 });
 
 // Login Handle
-router.post('/login', (req, res, next) => {
-	passport.authenticate('local', {
-		successRedirect: '/home',
-		failureRedirect: '/users/login',
-	})(req, res, next);
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/users/login",
+  })(req, res, next);
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
 });
 
 module.exports = router;
