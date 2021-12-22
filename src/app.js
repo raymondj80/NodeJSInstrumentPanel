3651d8d47ad39ff6a4e303df6d54343a7503a9ea
Raymond Jow <55035695+raymondj80@users.noreply.github.com>
2021-12-21 22:40:46 -0600

    login first iteration

--- src/app.js
+++ src/app.js
@@ -1,33 +1,71 @@
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const http = require("http").Server(app);
 const path = require("path");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require('connect-flash');
+const session = require("express-session");
 const io = require("socket.io")(http);
 require("dotenv").config({ path: "src/.env" });
-
+require("./config/passport")(passport);
 // Instantiate background process
 const bprocess = require("./process/bprocess.js");
-const routes = require("./routes/routes.js");
-require("./process/bprocess.js")(io);
+bprocess(io);
 
-const PORT = process.env.PORT;
+// Instantiate ports
+const PORT = process.env.PORT || 3000;
 const dbURI = process.env.DBURI;
 
-// Instantiate server and mongoDB
+// Connect to MongoDB
 mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) =>
-    http.listen(PORT, function () {
-      console.log("HTTP server started on port 3000");
-    })
+  .connect(
+    dbURI,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
   )
-  .catch((err) => console.log(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
+// Middleware
+app.use(expressLayouts);
+
+// app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+
+
+// Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+app.use("/users", require("./routes/users.js"));
+app.use("/", require("./routes/routes.js"));
 app.set("views", path.join(__dirname, "views"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
 app.use(express.static(__dirname + "/public"));
-app.use("/", routes);
 bprocess.backgroundProcess(1000);
+
+http.listen(PORT, console.log(`Server running on port ${PORT}`));
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,23 @@
-var express = require("express");
-var router = express.Router();
 
-router.get("/home", function (req, res, next) {
-  res.render("home");
-});
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Control Panel Home Page
+router.get('/home', ensureAuthenticated, (req, res) =>
+  res.render('home', {
+    user: req.user
+  })
+);
+
+// Control Panel Home Page
+router.get('/home1', ensureAuthenticated, (req, res) =>
+  res.render('home1', {
+    user: req.user
+  })
+);
 
 module.exports = router;
